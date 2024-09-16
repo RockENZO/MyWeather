a0e0fb699b36246347b171c3b8d2d654bdd503a8
RockENZO <gewang2004@gmail.com>
2024-09-16 11:42:14 +0930

    keep launch view simple with out check if statement

--- MyWeather/Views/LaunchView.swift
+++ MyWeather/Views/LaunchView.swift
@@ -8,36 +8,27 @@
 import SwiftUI
 
 struct LaunchView: View {
-    @State private var isActive = false
     @State private var size = 0.8
     @State private var opacity = 0.5
 
     var body: some View {
-            VStack {
-                Image(systemName: "cloud.sun.fill")
-                    .font(.system(size: 80))
-                    .foregroundColor(.blue)
-                Text("MyWeather")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundColor(.blue)
+        VStack {
+            Image(systemName: "cloud.sun.fill")
+                .font(.system(size: 80))
+                .foregroundColor(.blue)
+            Text("MyWeather")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(.blue)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .scaleEffect(size)
+        .opacity(opacity)
+        .onAppear {
+            withAnimation(.easeIn(duration: 1.2)) {
+                self.size = 1.0
+                self.opacity = 1.0
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .scaleEffect(size)
-            .opacity(opacity)
-            .onAppear {
-                withAnimation(.easeIn(duration: 1.2)) {
-                    self.size = 1.0
-                    self.opacity = 1.0
-                }
-            }
-            .onAppear {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-                    withAnimation {
-                        self.isActive = true
-                    }
-                }
-            
         }
     }
 }
