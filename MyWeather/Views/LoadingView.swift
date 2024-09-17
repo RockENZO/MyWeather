b1527ce5495091577e8f04cd7037f42cbc47ff19
RockENZO <gewang2004@gmail.com>
2024-09-17 10:48:04 +0930

    loading animation update

--- MyWeather/Views/LoadingView.swift
+++ MyWeather/Views/LoadingView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct LoadingView: View {
     @State private var isAnimating = false
-    
+    @State private var color: Color = .blue // Initial color
+    @State private var isColorAnimating = true // To control color animation
+
+    let colors: [Color] = [.blue, .red, .purple, .green, .orange] // Array of colors
+
     var body: some View {
         ZStack {
             Color.black.opacity(0.7)
@@ -20,18 +24,28 @@ struct LoadingView: View {
                     .resizable()
                     .scaledToFit()
                     .frame(width: 100, height: 100)
-                    .foregroundColor(.blue)
+                    .foregroundColor(color)
                     .scaleEffect(isAnimating ? 1.2 : 0.7)
                     .animation(Animation.easeInOut(duration: 0.8).repeatForever(autoreverses: true), value: isAnimating)
                 
                 Text("Loading Weather Data...")
                     .font(.title)
-                    .foregroundColor(.blue)
+                    .foregroundColor(color)
                     .bold()
                     .padding(.top, 20)
             }
             .onAppear {
                 self.isAnimating = true
+                
+                // Change color every 0.5 seconds indefinitely
+                let colorChangeInterval: TimeInterval = 0.5
+                Timer.scheduledTimer(withTimeInterval: colorChangeInterval, repeats: true) { timer in
+                    if self.isColorAnimating {
+                        withAnimation(.easeInOut(duration: 0.5)) {
+                            self.color = colors.randomElement() ?? .blue
+                        }
+                    }
+                }
             }
         }
     }
